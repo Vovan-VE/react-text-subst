77dd03ab1b19f03b2e5b59a5674b387b9c87a594
Vovan-VE <vovan-ve@yandex.ru>
2019-06-01 00:36:15 +0800

    Add .d.ts declaration

---
+++ index.d.ts
@@ -0,0 +1,29 @@
+import * as React from 'react';
+import * as PropTypes from 'prop-types';
+
+interface InlineItemComponentProps {
+    name: string;
+}
+
+interface BlockItemComponentProps extends InlineItemComponentProps {
+    children: React.ReactNode;
+}
+
+type InlineItemValue = React.ComponentType<InlineItemComponentProps> | React.ReactNode;
+type BlockItemValue = React.ComponentType<BlockItemComponentProps>;
+type ItemValue = React.ReactNode | InlineItemValue | BlockItemValue;
+
+interface TextSubstProps {
+    children?: string | null | undefined;
+    [key: string]: ItemValue;
+}
+
+declare class TextSubst extends React.Component<TextSubstProps, {}> {
+    static propTypes: {
+        children?: PropTypes.Validator<string>;
+        [key: string]: PropTypes.Validator<ItemValue>;
+    };
+    render(): React.ReactNode;
+}
+
+export default TextSubst;
